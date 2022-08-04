82db69d23757ae79349c516913da1e683de898f8
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-08-04 22:29:43 +0600

    Add files via upload

---
+++ Fixed Length List.dart
@@ -0,0 +1,9 @@
+void main() {
+  var mylist = new List<dynamic>.filled(3, 0); //length, fill
+  mylist[0] = 'Beautiful';
+  mylist[1] = 'Purple';
+  mylist[2] = 'Butterfly';
+
+  print(mylist);
+
+}
