a33dc2f8680c72a4adb085eddcc9627799643709
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-08-07 08:44:45 +0600

    Add files via upload

---
+++ Dart Generics for Map.dart
@@ -0,0 +1,8 @@
+void main(){
+var location = new Map<String,String>();
+
+location.addAll({"Continent":"Asia", "Country":"Bangladesh", "Division":"Sylhet", "District":"Sylhet"});
+
+print(location);
+
+}
