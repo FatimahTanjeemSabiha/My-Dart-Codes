901cbaf764d4f2ad8d579ace7acdb041d51a751a
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-08-07 22:02:35 +0600

    Add files via upload

---
+++ Method Override/Main.dart
@@ -0,0 +1,11 @@
+import 'Son.dart';
+
+void main(){
+  var object = new Son();
+  object.addTwo();
+  object.multiplyTwo();
+  object.color();
+  object.type();
+  
+
+}
---
+++ Method Override/Son.dart
@@ -0,0 +1,14 @@
+import 'Father.dart';
+
+class Son extends Father{
+
+void type(){
+  print("Butterfly");
+}
+
+@override
+void addTwo(){
+  print(20+30+40);
+}
+
+}
