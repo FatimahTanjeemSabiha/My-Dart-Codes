0881fc261f3a569ba5ac49076dd88a64c017d2a1
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-08-07 08:58:07 +0600

    Add files via upload

---
+++ Dart Generics for Hashmap.dart
@@ -0,0 +1,10 @@
+import 'dart:collection';
+
+void main(){
+
+var location = new HashMap<String, String>();
+location.addAll({"Continent":"Asia", "Country":"Bangladesh", "Division":"Sylhet", "District":"Sylhet"});
+
+print(location);
+
+}
