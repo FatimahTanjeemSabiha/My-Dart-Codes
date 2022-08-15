0801cca0ac6ab277c697a12378a253ab7ef136e3
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-08-15 14:01:39 +0600

    Add files via upload

---
+++ Use of Interface.dart
@@ -0,0 +1,34 @@
+void main(){
+
+}
+
+class normalClass implements interfaceClass{
+
+  void myClass(){
+
+    print("A");
+
+  }
+  
+  @override
+  void age() {
+   
+  }
+  
+  @override
+  void type() {
+  
+  }
+}
+
+class interfaceClass{
+
+  void type(){
+
+  }
+
+  void age(){
+    
+  }
+
+}
